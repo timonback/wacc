72a2103f7171fb30db26a19f7f45f41ee7c15e0f
likesmusik <3back@informatik.uni-hamburg.de>
2016-10-19 12:48:14 +0200

    Fix default mongo address

--- Solert/docker/webserver/app/services/ReactiveMongoApi.scala
+++ Solert/docker/webserver/app/services/ReactiveMongoApi.scala
@@ -6,7 +6,7 @@ import reactivemongo.api.{MongoDriver, MongoConnection}
 
 object MongoDB {
   def dbAddress = sys.env.get("MONGO_ADDRESS").getOrElse("localhost")
-  def dbPort = sys.env.get("MONGO_PORT").getOrElse("9042").toInt
+  def dbPort = sys.env.get("MONGO_PORT").getOrElse("27017").toInt
   def dbKeySpace = sys.env.get("MONGO_KEYSPACE").getOrElse("solert")
 
   val mongoUri = "mongodb://" + dbAddress + ":" + dbPort + "/" + dbKeySpace;
